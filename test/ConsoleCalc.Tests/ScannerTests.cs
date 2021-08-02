df9326b5c0cb610bd36a54073b504f5424463f77
Jan Urbaś <janurbas39@gmail.com>
2021-08-02 10:39:56 +0200

    Created parenScanTest that tests if tokens of parenthesis were created correctly

--- test/ConsoleCalc.Tests/ScannerTests.cs
+++ test/ConsoleCalc.Tests/ScannerTests.cs
@@ -6,29 +6,22 @@ namespace ConsoleCalc.Tests
 {
     public class ScannerTests
     {
-        // Scanner scan = new Scanner("5+2");
-        // scan.scanTokens();
-        // foreach(var token in scan.tokens){
-        //     System.Console.WriteLine(token);
-        // }
-
         [Fact]
         void simpleCalculationTest() 
         {
             // arrange
                 Scanner scan = new Scanner("5+2");
-            // act
-                scan.scanTokens();
                 List<Token> correctTokens = new List<Token>() 
                 {
                     {new Token(Token.TokenType.NUMBER, "5", Convert.ToDouble(5))},
                     {new Token(Token.TokenType.PLUS, "+", null)},
                     {new Token(Token.TokenType.NUMBER, "2", Convert.ToDouble(2))}
                 };
+            // act
+                scan.scanTokens();
             // assert
                 for(var i = 0; i < correctTokens.Count; i++)
                 {
-
                     Assert.Equal(correctTokens[i].type, scan.tokens[i].type);
                     Assert.Equal(correctTokens[i].lexeme, scan.tokens[i].lexeme);
                     Assert.Equal(correctTokens[i].literal, scan.tokens[i].literal);
@@ -38,7 +31,26 @@ namespace ConsoleCalc.Tests
         void ParenCalcTest()
         {
             // arrange
+                Scanner parenScan = new Scanner("2*(3+2)");
+                List<Token> correctParenTokens = new List<Token>() 
+                {
+                    {new Token(Token.TokenType.NUMBER, "2", Convert.ToDouble(2))},
+                    {new Token(Token.TokenType.STAR, "*", null)},
+                    {new Token(Token.TokenType.LEFT_PAREN, "(", null)},
+                    {new Token(Token.TokenType.NUMBER, "3", Convert.ToDouble(3))},
+                    {new Token(Token.TokenType.PLUS, "+", null)},
+                    {new Token(Token.TokenType.NUMBER, "2", Convert.ToDouble(2))},
+                    {new Token(Token.TokenType.RIGHT_PAREN, ")", null)}                  
+                };
+            // act
+                parenScan.scanTokens();
+            // assert
+                for(var i = 0; i < correctParenTokens.Count; i++)
+                {
+                    Assert.Equal(correctParenTokens[i].type, parenScan.tokens[i].type);
+                    Assert.Equal(correctParenTokens[i].lexeme, parenScan.tokens[i].lexeme);
+                    Assert.Equal(correctParenTokens[i].literal, parenScan.tokens[i].literal);
+                }
         }
-
     }
 }
