77f9693850e9c5b27f8851ebbb041559b73bf58c
Jan Urbaś <janurbas39@gmail.com>
2021-08-23 15:14:04 +0200

    Replaced switch statament into second InlineData argument in ActionTests.cs

--- test/ConsoleCalc.Tests/ActionTests.cs
+++ test/ConsoleCalc.Tests/ActionTests.cs
@@ -12,42 +12,27 @@ namespace ConsoleCalc.Tests
     public class ActionTests
     {
         [Theory]
-        [InlineData("2+6:2*3")]
-        [InlineData("7-2*0+5:5-2")]
-        [InlineData("2*(1+3)")]
-        [InlineData("2(1 + 3)")]
-        [InlineData("-99 + 1)")]
-        [InlineData("(-99+1)*1=")]
-        [InlineData("2^5")]
-        [InlineData("2 ^ 5")]
-        [InlineData("2(1+5^2)^3")]
-        [InlineData("(1 + 3)2")]
-        [InlineData("25%*4")] // percentage of number
-        [InlineData("125%10")] // modulo
-        [InlineData("2(25%*100+5^2)^3%13")]
-        private void Should_Be_Correctly_Calculated(string source)
+        [InlineData("2+6:2*3", 11)]
+        [InlineData("7-2*0+5:5-2", 6)]
+        [InlineData("2*(1+3)", 8)]
+        [InlineData("2(1 + 3)", 8)]
+        [InlineData("-99 + 1)", -98)]
+        [InlineData("(-99+1)*1=", -98)]
+        [InlineData("2^5", 32)]
+        [InlineData("2 ^ 5", 32)]
+        [InlineData("2(1+5^2)^3", 35152)]
+        [InlineData("(1 + 3)2", 8)]
+        [InlineData("25%*4", 1)] // percentage of number
+        [InlineData("125%10", 5)] // modulo
+        [InlineData("2(25%*100+5^2)^3%13", 10)]
+        private void Should_Be_Correctly_Calculated(string source, double result)
         {
             // arrange
             Scanner scan = new Scanner(source);
             Parser parsedScan = new Parser(scan.ScanTokens());
 
             // assert
-            switch (source)
-            {
-                case "2+6:2*3": Assert.Equal(11, parsedScan.Result()); break;
-                case "7-2*0+5:5-2": Assert.Equal(6, parsedScan.Result()); break;
-                case "2*(1+3)": Assert.Equal(8, parsedScan.Result()); break;
-                case "2(1 + 3)": Assert.Equal(8, parsedScan.Result()); break;
-                case "- 99 + 1": Assert.Equal(-98, parsedScan.Result()); break;
-                case "(-99+1)*1=": Assert.Equal(-98, parsedScan.Result()); break;
-                case "2^5": Assert.Equal(32, parsedScan.Result()); break;
-                case "2 ^ 5": Assert.Equal(32, parsedScan.Result()); break;
-                case "2(1+5^2)^3": Assert.Equal(35152, parsedScan.Result()); break;
-                case "(1 + 3)2": Assert.Equal(8, parsedScan.Result()); break;
-                case "25%*4": Assert.Equal(1, parsedScan.Result()); break;
-                case "125%10": Assert.Equal(5, parsedScan.Result()); break;
-                case "2(25%*100+5^2)^3%13": Assert.Equal(10, parsedScan.Result()); break;
-            }
+            Assert.Equal(result, parsedScan.Result());
         }
     }
 }
